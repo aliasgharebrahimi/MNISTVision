a416dc60f12bab8fca94a18a29617276585077f8
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-08-14 20:04:12 +0330

    feat(train): add func train

---
+++ tarin.py
@@ -0,0 +1,25 @@
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def train(model, data, dataloader, optimizer, loss_fun ):
+
+    model.train()
+    model = model.to(device)
+
+    total_loss = 0
+
+    for batch_idx, (data, target) in enumerate(dataloader):
+
+        data, target = data.to(device), target.to(device)
+
+        pred = model(data)
+        loss = loss_fun(pred, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(dataloader)
