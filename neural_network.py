1065f4ea2b7dfcd82d9a58d57355ceda7fea985a
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-08-14 18:36:38 +0330

    feat(neural_network): add neural_network

---
+++ neural_network.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+class MNISTNet(nn.Module):
+    def __init__(self):
+        super.__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=3)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(in_features=300, out_features=10)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+
+        return x
