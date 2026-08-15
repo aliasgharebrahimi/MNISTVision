d22df3f565ae75e425bae3682ed863bdfa088936
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-08-15 17:33:07 +0330

    feat(dataloader): add num_workers

--- dataloader.py
+++ dataloader.py
@@ -4,11 +4,13 @@ from dataset import train_data, eval_data
 train_dataloader = DataLoader(
     train_data,
     batch_size=64,
-    shuffle=True
+    shuffle=True,
+    num_workers=0
 )
 
 eval_dataloader = DataLoader(
     train_data,
     batch_size=64,
-    shuffle=True
+    shuffle=True,
+    num_workers=0
 )
