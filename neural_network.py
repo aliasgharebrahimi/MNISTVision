b6f936e5cd508a87e2fcd3aefacd3d8797f6f238
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-08-16 13:06:54 +0330

    fix(neural_network): Fix the issue of missing parentheses in the super call.

--- neural_network.py
+++ neural_network.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 class MNISTNet(nn.Module):
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3)
         self.relu1 = nn.ReLU()
