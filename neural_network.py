61e21f7c1a68346b3f31a15b909f6c050387ad5f
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-08-17 11:38:12 +0330

    feat(neural_network): add stride

--- neural_network.py
+++ neural_network.py
@@ -4,10 +4,10 @@ class MNISTNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3, stride=1)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=3, stride=1)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2)
         self.flatten = nn.Flatten()
