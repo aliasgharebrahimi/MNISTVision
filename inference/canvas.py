7eaf7cc1a447d7b5401044a0e4b8d44571f9c9d4
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-09-05 13:58:25 +0330

    feat(inference): add canvas

--- inference/canvas.py
+++ inference/canvas.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
-from preprocessing import preprocess
+
+from inference.preprocessing import preprocess
+from inference.predictor import predict
+
 
 canvas = np.ones((400, 400), dtype=np.uint8) * 255
 
 drawing = False
+prediction = None
 
 
 def draw(event, x, y, flags, param):
@@ -25,16 +29,31 @@ cv2.setMouseCallback("MNISTVision", draw)
 
 
 while True:
-    cv2.imshow("MNISTVision", canvas)
+    display_canvas = canvas.copy()
+
+    if prediction is not None:
+        cv2.putText(
+            display_canvas,
+            f"Prediction: {prediction}",
+            (10, 35),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.9,
+            0,
+            2
+        )
+
+    cv2.imshow("MNISTVision", display_canvas)
 
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("c") or key == ord("C"):
         canvas[:] = 255
+        prediction = None
 
     elif key == ord("p") or key == ord("P"):
         image = preprocess(canvas)
-        print(image.shape)
+        prediction = predict(image)
+        print(f"Prediction: {prediction}")
 
     elif key == 27:
         break
--- inference/predictor.py
+++ inference/predictor.py
@@ -5,8 +5,11 @@ from neural_network import MNISTNet
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = MNISTNet()
-model.load_state_dict(torch.load("mnist_model.pth", map_location=device))
+from pathlib import Path
 
+MODEL_PATH = Path(__file__).resolve().parent.parent / "mnist_model.pth"
+
+model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
 model.to(device)
 model.eval()
 
