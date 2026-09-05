d90376bb40ca6743aeddf25b39ab30828b03b304
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-09-05 14:12:39 +0330

    feat(inference): Add the 'show Result' and 'delete' buttons.

--- inference/canvas.py
+++ inference/canvas.py
@@ -12,9 +12,23 @@ prediction = None
 
 
 def draw(event, x, y, flags, param):
-    global drawing
+    global drawing, prediction
 
     if event == cv2.EVENT_LBUTTONDOWN:
+
+        button = handle_button(x, y)
+
+        if button == "predict":
+            image = preprocess(canvas)
+            prediction = predict(image)
+            print(f"Prediction: {prediction}")
+            return
+
+        elif button == "clear":
+            canvas[:] = 255
+            prediction = None
+            return
+
         drawing = True
 
     elif event == cv2.EVENT_MOUSEMOVE and drawing:
@@ -23,13 +37,47 @@ def draw(event, x, y, flags, param):
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
 
+def handle_button(x, y):
+    # Predict button
+    if 250 <= x <= 390 and 350 <= y <= 390:
+        return "predict"
 
+    # Clear button
+    if 100 <= x <= 240 and 350 <= y <= 390:
+        return "clear"
+
+    return None
+
+def draw_buttons(image):
+    cv2.rectangle(image, (100, 350), (240, 390), 0, -1)
+    cv2.rectangle(image, (250, 350), (390, 390), 0, -1)
+
+    cv2.putText(
+        image,
+        "Clear",
+        (135, 377),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.6,
+        255,
+        2
+    )
+
+    cv2.putText(
+        image,
+        "Predict",
+        (275, 377),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.6,
+        255,
+        2
+    )
 cv2.namedWindow("MNISTVision")
 cv2.setMouseCallback("MNISTVision", draw)
 
 
 while True:
     display_canvas = canvas.copy()
+    draw_buttons(display_canvas)
 
     if prediction is not None:
         cv2.putText(
