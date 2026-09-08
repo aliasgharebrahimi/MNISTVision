f298f844ada678cfa5d719f08407b24d64fee264
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-09-08 19:46:22 +0330

    feat(eval): add acc to func eval

--- eval.py
+++ eval.py
@@ -1,18 +1,22 @@
 import torch
 from tarin import device
+from accuracy import accuracy
 
-def eval(model, data, dataloader, loss_func, device):
+def eval(model, dataloader, loss_func, device):
 
     model.eval()
     model = model.to(device)
 
     total_loss = 0
+    total_accuracy = 0
 
-    with torch.norm():
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(dataloader):
 
-        pred = model(data)
-        loss = loss_func(pred, data)
+            pred = model(data)
+            loss = loss_func(pred, target)
 
-        total_loss += loss.item()
+            total_loss += loss.item()
+            total_accuracy += accuracy(pred, target)
 
-    return total_loss / len(dataloader)
+    return total_loss / len(dataloader), total_accuracy / len(dataloader)
