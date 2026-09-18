3ee42bbadbcc9e0c4371b58ea0f37a59d218b716
Ali Asghar Ebrahimi <aliasghare1.ai@gmail.com>
2026-09-18 16:29:53 +0330

    feat(neural_network): add class mnistnet to fiel mnsitnet

--- neural_network/blocks.py
+++ neural_network/blocks.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, padding = 0, stride = 1):
+    def __init__(self, in_channels, out_channels, kernel_size, padding = 0, stride = 1, kaiming_normal = False):
         super().__init__()
 
         self.conv = nn.Conv2d(
@@ -12,12 +12,17 @@ class ConvBlock(nn.Module):
             padding=padding
         )
 
+        if kaiming_normal:
+            nn.init.kaiming_normal_(self.conv.weight, nonlinearity="leaky_relu", a=0.01)
+
         self.bn = nn.BatchNorm2d(num_features=out_channels)
         self.leaky_relu = nn.LeakyReLU()
         self.pool = nn.MaxPool2d(kernel_size=2)
 
-        def forward(self, x):
-            x = self.conv(x)
-            x = self.bn(x)
-            x = self.leaky_relu(x)
-            return self.pool(x)
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.leaky_relu(x)
+        x = self.pool(x)
+
+        return x
